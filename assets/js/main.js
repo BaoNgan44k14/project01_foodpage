9311024611ee0fe8e08220cf5675395db6d3eccb
BaoNgan44k14 <78129988+BaoNgan44k14@users.noreply.github.com>
2021-10-31 19:19:09 +0700

    31/10

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,29 @@ function fadeOut() {
     setInterval(loader, 3000);
 }
 window.onload = fadeOut();
+
+// slide
+
+var swiper = new Swiper(".feedback-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+          slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
